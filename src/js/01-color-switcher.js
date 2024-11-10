d04e8e9390b77e23540611383c8da4f4b2306c94
DutuGabriel <gdutu94@gmail.com>
2024-11-10 19:06:44 +0200

    Added exercises 1, 2, 3 and verified them for submission

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let colorChangeInterval;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startColorChange() {
+  colorChangeInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function stopColorChange() {
+  clearInterval(colorChangeInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.target.elements;
+
+  let firstDelay = Number(delay.value);
+  const delayStep = Number(step.value);
+  const amountPromises = Number(amount.value);
+
+  for (let i = 1; i <= amountPromises; i++) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    firstDelay += delayStep;
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const promiseResult = { position, delay };
+
+      if (shouldResolve) {
+        resolve(promiseResult);
+      } else {
+        reject(promiseResult);
+      }
+    }, delay);
+  });
 }
